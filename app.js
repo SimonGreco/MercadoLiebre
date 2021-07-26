dc36a2619ca93dfc7de599fd76afd2885f87224c
SimonGreco <simongreco60@gmail.com>
2021-07-26 19:35:52 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static("public"));
+
+app.listen(3030, function(){
+    console.log("servidor levantado en el puerto 3000")
+});
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+app.get("/register", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+app.get("/login", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+app.get("/h", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/headder.html"))
+});
+
+
+
+
